ec6a3c40f998b4c7c73a7bb631f2f86538b460cc
Jan Mrkos <mrkosjan@gmail.com>
2020-12-03 14:44:28 +0100

    Started setting up problems differently

---
+++ src/problem_instances/linear_problem.jl
@@ -0,0 +1,11 @@
+"""
+General definition of a linear problem that is instantiated by setting its parameters.
+
+How should it look? 
+ - Function returning and MDP? 
+ - Its own struct?
+"""
+
+function linear_problem()
+
+end
