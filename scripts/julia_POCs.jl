0cd582c3ffdbc8221a2a1a1fa91d4abf1c3d6415
Jan Mrkos <mrkosjan@gmail.com>
2020-12-07 21:59:30 +0100

    Added vector interface POC

---
+++ scripts/julia_POCs.jl
@@ -0,0 +1,42 @@
+# Assigning concrete function call to a method saves the function, not the result.
+function factorial(n)
+    if n>1
+        return n*factorial(n-1)
+    elseif n==1
+        return 1
+    end
+end
+
+using BenchmarkTools
+
+fn = factorial(15)
+
+
+
+special() = factorial(15)
+special2() = fn
+
+@benchmark factorial(15)
+@benchmark special()
+@benchmark special2()
+
+# define vector interface for custom struct
+module vectorinterface_poc
+
+using StaticArrays
+
+struct mysv{Size} <: StaticArray{Tuple{Size}, Bool, 1}
+    v::SVector{Size, Bool}
+    meta::String
+end
+
+@inline Tuple(m::mysv) = m.v.data
+StaticArrays.@propagate_inbounds function Base.getindex(m::mysv, i::Int) 
+    m.v.data[i]
+end
+
+a = mysv{2}(SVector(false, false), "aha")
+b = SVector(true, true)
+
+@show b+a
+end
