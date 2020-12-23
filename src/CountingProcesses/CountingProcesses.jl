aa85000aced78764275846bf52e31fb0731fcaeb
Jan Mrkos <mrkosjan@gmail.com>
2020-12-23 17:06:43 +0100

    Fix the counting processes

--- src/CountingProcesses/CountingProcesses.jl
+++ src/CountingProcesses/CountingProcesses.jl
@@ -4,37 +4,9 @@ using Distributions
 using StaticArrays
 using Random
 
-export _rand!
+export DiscreteCountingProcess, BernoulliScheme
 
-
-"""
-Abstract type for memoryless random processes
-"""
-abstract type MemorylessCountingProcess end
-
-"""
-    rand(bp::MemorylessCountingProcess)
-    
-Sample the next event in the process (time or timestep AND outcome)
-
-    rand(bp::MemorylessCountingProcess, t::Number)
-
-Sample event at time t. Works only for memoryless processes.
-"""
-function rand end
-
-
-# """
-#     reset!(bp::MemorylessCountingProcess)
-
-# Reset the state of the process to the start
-# """
-# function reset! end
-
-
- 
-
-# include("poisson_process.jl")
+include("poisson_process.jl")
 include("bernoulli_process.jl")
 
 end
