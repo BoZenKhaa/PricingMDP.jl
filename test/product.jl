219f297cb8eeddb9e9e12fe4204425c0068ea3b8
Jan Mrkos <mrkosjan@gmail.com>
2020-12-29 10:50:17 +0100

    Tests for product

---
+++ test/product.jl
@@ -0,0 +1,23 @@
+using StaticArrays
+using PMDPs
+using Test
+
+
+@testset "product.jl" begin
+
+    p1 = PMDPs.Product(SA[true, false], 6)
+    @test typeof(p1) <: PMDPs.Product
+    
+    p2 = PMDPs.Product(SA[true, true], 3)
+    @test p1+p2 == [2,1] 
+
+    products = [PMDPs.Product(SA[true, false], 6), 
+                PMDPs.Product(SA[false, true], 8), 
+                PMDPs.Product(SA[true, true], 6)]
+    @test PMDPs.are_unique(products)
+
+    products_not_unique = [PMDPs.Product(SA[true, false], 6), 
+                PMDPs.Product(SA[false, true], 8), 
+                PMDPs.Product(SA[false, true], 6)]
+    @test !PMDPs.are_unique(products_not_unique)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,10 @@ include("mdp_instances.jl")
 
 @testset "PricingMDP.jl" begin
     include("CountingProcesses/runtest.jl")
+    include("product.jl")
     include("PMDPProblem.jl")
     include("PMDPg.jl")
+    # include("PMDPe.jl")
     include("PMDP.jl")
     # include("trace_generation.jl")
     # include("policy_tools.jl")
