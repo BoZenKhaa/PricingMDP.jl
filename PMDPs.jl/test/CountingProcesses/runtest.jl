179a2bb476f99325b17f004be294589acfd16fba
BoZenKhaa <mrkosjan@gmail.com>
2022-10-03 14:11:22 +0200

    Revert "Undo separation of PMDPs.jl into separate repo"
    This reverts commit 5c6dad5d2b2177eb3c1d0056eff2483d393768c5.

--- PMDPs.jl/test/CountingProcesses/runtest.jl
+++
@@ -1,6 +0,0 @@
-using PMDPs
-using Test
-
-@testset "CountingProcesses.jl" begin
-    include("bernoulli_process.jl")
-end
--- PMDPs.jl/test/trace_generation.jl
+++
@@ -1,9 +0,0 @@
-using Random
-using POMDPSimulators
-
-@testset "trace_generation.jl" begin
-    pp = simple_pp()
-    mg = PMDPs.PMDPg(pp)
-    h = PMDPs.simulate_trace(mg, MersenneTwister(12))
-    @test isa(h, SimHistory)
-end
