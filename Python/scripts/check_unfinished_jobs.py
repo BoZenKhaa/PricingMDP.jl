136b032094f803bce2cc5d47842b83730be4b8b3
Jan Mrkos <mrkosjan@gmail.com>
2024-09-10 15:17:59 +0200

    Unfinished jobs int CMD script

--- Python/scripts/check_unfinished_jobs.py
+++ Python/scripts/check_unfinished_jobs.py
@@ -1,25 +1,39 @@
+import argparse
 import logging
 from pathlib import Path
 
-data = Path("/mnt/data/mobility/MDPPricing/data/ev_variable_resources")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog="RCI-MDPPricing_jobs_checker",
+        description="Script to check which jobs are unfinished on RCI cluster",
+    )
+    parser.add_argument("--results_path", default="/mnt/data/mobility/MDPPricing/data",
+                        help="Path to results/experiment configs.")
+    # parser.add_argument("--instances_path", default="/home/fiedlda1/Experiment Data/DARP/final/Instances",
+    #                     help="Path to instances.")
+    parser.add_argument("--output", default="unfinished_runs.txt",
+                        help='path to the output file. If empty, log into console only. (Default: "unfinished_runs.txt")')
+    args = parser.parse_args()
 
-results_path = data
+    results_path = args.results_path
+    output = args.output
 
-logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
 
-# for instance_config_path in instances_path.rglob("*/config.yaml"):
-#     logging.info(instance_config_path)
+    # for instance_config_path in instances_path.rglob("*/config.yaml"):
+    #     logging.info(instance_config_path)
 
-unfinished_runs = []
-for result_config_path in results_path.rglob("**/config*.yaml"):
-    if (result_config_path.parent / f"{result_config_path.stem}_result.csv").is_file():
-        # logging.info(f"\t-SOLUTION- \t{result_config_path}")
-        pass
-    else:
-        logging.info(f"\tXXX-NOT-XXX\t{result_config_path}")
-        unfinished_runs.append(result_config_path)
+    unfinished_runs = []
+    for result_config_path in results_path.rglob("**/config*.yaml"):
+        if (result_config_path.parent / f"{result_config_path.stem}_result.csv").is_file():
+            # logging.info(f"\t-SOLUTION- \t{result_config_path}")
+            pass
+        else:
+            logging.info(f"\tXXX-NOT-XXX\t{result_config_path}")
+            unfinished_runs.append(result_config_path)
 
-logging.info(f"Found {len(unfinished_runs)} unfinished jobs.")
+    logging.info(f"Found {len(unfinished_runs)} unfinished jobs.")
 
-with open("unfinished_runs_8GB.txt", "wt", encoding="utf-8") as f:
-    f.write("\n".join(map(str, unfinished_runs)))
+    if output:
+        with open(output, "wt", encoding="utf-8") as f:
+            f.write("\n".join(map(str, unfinished_runs)))
